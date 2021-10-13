e2e7c67ee9be101db92dd0788141e49fb485d1eb
Ivan Lashin <iw.lasgin2011@gmail.com>
2021-10-13 18:35:58 +0300

    add max time between auto save

--- Assets/SaveManagerPackage/Scripts/AutoSaveController.cs
+++ Assets/SaveManagerPackage/Scripts/AutoSaveController.cs
@@ -1,19 +1,45 @@
+using System;
 using UnityEngine;
 
 namespace SaveManagerPackage.Scripts
 {
     public class AutoSaveController : MonoBehaviour
     {
+        private const float MinTimeBetweenSaves = 10f;
+
+        private static float _currentTimeBetweenSaves = MinTimeBetweenSaves;
+        
         private void Awake()
         {
             DontDestroyOnLoad(this);
+        }
+
+        private void Update()
+        {
+            _currentTimeBetweenSaves += Time.deltaTime;
+        }
+
+        private void TryAutoSave()
+        {
+            if (!CanAutoSave())
+            {
+                return;
+            }
+            
+            SaveManager.SaveAll();
+            _currentTimeBetweenSaves = 0;
+        }
+
+        private bool CanAutoSave()
+        {
+            return _currentTimeBetweenSaves >= MinTimeBetweenSaves;
         }
 
         private void OnApplicationFocus(bool hasFocus)
         {
             if (!hasFocus)
             {
-                SaveManager.SaveAll();
+                TryAutoSave();
             }
         }
 
@@ -21,13 +47,13 @@ namespace SaveManagerPackage.Scripts
         {
             if (pauseStatus)
             {
-                SaveManager.SaveAll();
+                TryAutoSave();
             }
         }
 
         public void OnApplicationQuit()
         {
-            SaveManager.SaveAll();
+            TryAutoSave();
         }
     }
 }
