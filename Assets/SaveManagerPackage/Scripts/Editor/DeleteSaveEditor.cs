e2df358f92b1c919e7031088332c03d0267f6515
Ivan Lashin <iw.lasgin2011@gmail.com>
2021-10-15 15:59:25 +0300

    add delete save

---
+++ Assets/SaveManagerPackage/Scripts/Editor/DeleteSaveEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class DeleteSaveEditor : MonoBehaviour
+{
+    [MenuItem("Tools/Game/DeleteSave")]
+    private static void DeleteSave()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
